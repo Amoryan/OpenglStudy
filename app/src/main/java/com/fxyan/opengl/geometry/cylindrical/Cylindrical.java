bffe336250ee9eabc9f2115cf51c389b97f1f2af
yanfangxiong <yanfangxiong1226@163.com>
2019-03-25 17:04:16 +0800

    修改圆柱的绘制

--- app/src/main/java/com/fxyan/opengl/geometry/cylindrical/Cylindrical.java
+++ app/src/main/java/com/fxyan/opengl/geometry/cylindrical/Cylindrical.java
@@ -2,9 +2,14 @@ package com.fxyan.opengl.geometry.cylindrical;
 
 import android.opengl.GLES20;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import com.fxyan.opengl.base.ModelImpl;
 import com.fxyan.opengl.utils.GLESUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 
 /**
  * @author fxYan
@@ -15,9 +20,28 @@ public final class Cylindrical
     private final int PER_VERTEX_SIZE = 3;
     private final int PER_VERTEX_STRIDE = PER_VERTEX_SIZE * PER_FLOAT_BYTES;
 
+    private FloatBuffer vertexBuffer;
+    private float[] vertex;
+
     private int programHandle;
 
     public Cylindrical() {
+        int temp = 360 * 10;
+        vertex = new float[temp * 2 * PER_VERTEX_SIZE];
+        float perDegree = 360f / temp;
+        for (int i = 0; i < temp; i++) {
+            double degree = Math.toRadians(perDegree * i);
+            vertex[i * PER_VERTEX_SIZE * 2] = vertex[3 + i * PER_VERTEX_SIZE * 2] = 0.5f * (float) Math.cos(degree);
+            vertex[1 + i * PER_VERTEX_SIZE * 2] = -0.5f;
+            vertex[2 + i * PER_VERTEX_SIZE * 2] = vertex[5 + i * PER_VERTEX_SIZE * 2] = 0.5f * (float) Math.sin(degree);
+            vertex[4 + i * PER_VERTEX_SIZE * 2] = 0.5f;
+        }
+
+        vertexBuffer = ByteBuffer.allocateDirect(vertex.length * PER_FLOAT_BYTES)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(vertex);
+        vertexBuffer.position(0);
     }
 
     @Override
@@ -44,6 +68,23 @@ public final class Cylindrical
 
         GLES20.glUseProgram(programHandle);
 
+        Matrix.setIdentityM(modelMatrix, 0);
+        long time = SystemClock.uptimeMillis() % 10000L;
+        float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
+        Matrix.rotateM(modelMatrix, 0, angleInDegrees, 1, 1, 1);
+        Matrix.multiplyMM(mvMatrix, 0, viewMatrix, 0, modelMatrix, 0);
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, mvMatrix, 0);
+        int mvpMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
+        GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0);
+
+        vertexBuffer.position(0);
+        int positionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+        GLES20.glVertexAttribPointer(positionHandle, PER_VERTEX_SIZE, GLES20.GL_FLOAT, false, PER_VERTEX_STRIDE, vertexBuffer);
+
+        GLES20.glDrawArrays(GLES20.GL_LINES, 0, vertex.length / PER_VERTEX_SIZE);
+
+        GLES20.glDisableVertexAttribArray(positionHandle);
     }
 
 }
